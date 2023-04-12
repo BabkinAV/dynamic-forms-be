4bc4a43646f1c99f0c1db60eb52a40e87db56877
Andrey Babkin <babkin.andrey@gmail.com>
2023-04-12 15:46:21 +0300

    changed req body

--- server.js
+++ server.js
@@ -11,58 +11,71 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   if (req.method === 'POST') {
+    // console.log(req.body);
     const createdAt = new Date();
-    req.body.createdAt = createdAt.toISOString();
-    req.body.updatedAt = createdAt.toISOString();
 
-		
-    req.body.id = uuidv4().slice(0, 12);
+    const newId = uuidv4().slice(0, 12);
 
-    req.body.org = {
-      id: '131b9d78-ad58-415f-aab4-779a9e87edfd',
-      name: 'Название организации',
-      inn: '7709655212',
-      kpp: '772901001',
-      ogrn: '1026101794313',
-      addr: 'Юридический адрес',
-      bank_accounts: [
-        {
-          id: 'c0ca02e9-be90-456b-b77c-82189c7651ae',
-          name: '2',
-          bik: '123456783',
-          account_number: '12345678901234567891',
-          corr_account_number: '09876543210987654321',
-          is_default: true,
-          created_at: '2023-03-31T13:27:06Z',
-          updated_at: '2023-03-31T13:35:58Z',
-        },
+    const name = req.body.name ?? '';
+    const email = req.body.email ?? '';
+    const deferral_days = req.body.deferral_days ?? 0;
+    const credit_limit = req.body.credit_limit ?? 0;
 
-        {
-          id: '23c3a0d6-c13f-45b1-9143-ef27af1b6d44',
-          name: 'Название1',
-          bik: '123456783',
-          account_number: '12345678901234567890',
-          corr_account_number: '09876543210987654321',
-          is_default: false,
-          created_at: '2023-03-31T13:27:06Z',
-          updated_at: '2023-03-31T13:35:58Z',
-        },
-      ],
-      created_at: '2023-03-31T13:27:06Z',
-      updated_at: '2023-03-31T13:35:58Z',
+    req.body = {
+      id: newId,
+      name,
+      email,
+      deferral_days,
+      org: {
+        id: '131b9d78-ad58-415f-aab4-779a9e87edfd',
+        name: 'Название организации',
+        inn: '7709655212',
+        kpp: '772901001',
+        ogrn: '1026101794313',
+        addr: 'Юридический адрес',
+        bank_accounts: [
+          {
+            id: 'c0ca02e9-be90-456b-b77c-82189c7651ae',
+            name: '2',
+            bik: '123456783',
+            account_number: '12345678901234567891',
+            corr_account_number: '09876543210987654321',
+            is_default: true,
+            created_at: '2023-03-31T13:27:06Z',
+            updated_at: '2023-03-31T13:35:58Z',
+          },
+          {
+            id: '23c3a0d6-c13f-45b1-9143-ef27af1b6d44',
+            name: 'Название1',
+            bik: '123456783',
+            account_number: '12345678901234567890',
+            corr_account_number: '09876543210987654321',
+            is_default: false,
+            created_at: '2023-03-31T13:27:06Z',
+            updated_at: '2023-03-31T13:35:58Z',
+          },
+        ],
+        created_at: '2023-03-31T13:27:06Z',
+        updated_at: '2023-03-31T13:35:58Z',
+      },
+      balance: {
+        currency: 'RUB',
+        current_amount: 90000,
+        credit_limit,
+        available_amount: 90000,
+      },
+      metadata: {
+        key1: 'val1',
+      },
+      created_at: createdAt.toISOString(),
+      updated_at: createdAt.toISOString(),
+      status: 'active',
+      invoice_prefix: 'L1RFJG',
+      invoice_emails: ['123@mail.com'],
     };
-    req.body.metadata = {
-      key1: 'val1',
-    };
-    req.body.balance.current_amount = 9000;
-    req.body.balance.available_amount = 9000;
 
-
-    req.body.metadata = {};
-    req.body.status = 'active';
-    req.body.invoice_prefix = 'L1RFJG';
-    req.body.invoice_emails = 'test@test.com';
   }
+
   // Continue to JSON Server router
   next();
 });
